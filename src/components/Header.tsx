5deec3717cc46fe465d551fe58ee0253fb29cbd5
EvgenyLukashevich <info.elukas@gmail.com>
2023-05-04 00:42:11 +0300

    Lesson-14

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import Classes from './Header.module.css'
 
 export const Header = () => {
     return(
-        <header className='header'>
+        <header className= {Classes.header}>
             <img src="https://logos-world.net/wp-content/uploads/2020/04/Huawei-Logo-700x394.png"  alt="logo"/>
         </header>
     )
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,22 @@
+// @ts-ignore
 import React from "react";
+import Classes from './Navbar.module.css'
+
+// let c1 = 'item'
+// let c2 = 'active'
+// let classes = c1 + ' ' + c2 //'item active'
+ let classesNew = `${Classes.item} ${Classes.active}`
 
 export const Navbar = () => {
     return (
-        <nav className='nav'>
-            <div>Profile</div>
+        <nav className= {Classes.nav}>
+            <div className = {classesNew}>
+                <div>  Profile</div>
             <div>Messages</div>
             <div>New</div>
             <div>Music</div>
             <div>Settings</div>
+            </div>
         </nav>
     )
 }

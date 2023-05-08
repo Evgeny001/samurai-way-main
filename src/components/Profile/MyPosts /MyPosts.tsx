d7a538c5fb9012bcf7237fc08d324cb93befed1b
EvgenyLukashevich <info.elukas@gmail.com>
2023-05-08 13:57:22 +0300

    Lesson-16

--- src/components/Profile/MyPosts /MyPosts.tsx
+++ src/components/Profile/MyPosts /MyPosts.tsx
@@ -2,13 +2,12 @@ import React from "react";
 import {Pocts} from "./Pocts/Pocts";
 
 export const MyPosts = () => {
+  
     return(
 
             <div >
-                <Pocts/>
-                <Pocts/>
-                <Pocts/>
-                <Pocts/>
+                <Pocts message={'Hi, how are you'} likes={15} />
+               <Pocts message={'It is my first post'} likes={20}/>
         </div>
     )
 }
--- src/components/Profile/MyPosts /Pocts/Pocts.tsx
+++ src/components/Profile/MyPosts /Pocts/Pocts.tsx
@@ -1,14 +1,19 @@
 import React from "react";
 import Classes from './Pocts.module.css'
 
-export const Pocts = () => {
+type PostsPropsType = {
+   message: string
+    likes : number
+}
+
+export const Pocts:React.FC<PostsPropsType> = (props) => {
     return(
         <div>
             <div className={Classes.item}>
                <img
                    src='https://assets.survivalinternational.org/pictures/489/4116455578-1f8d781f66-b_article_column@2x.jpg' alt=""/>
-            <div className={Classes.item}>post1</div>
-            <div><span>like</span></div>
+            <div className={Classes.item}>{props.message}</div>
+            <div><span>like {props.likes}</span></div>
             </div>
         </div>
 

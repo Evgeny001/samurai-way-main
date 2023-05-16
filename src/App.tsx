c82acfa5ea3fdf1f72825bf55c84bade47eb72b7
EvgenyLukashevich <info.elukas@gmail.com>
2023-05-16 12:47:13 +0300

    Lesson-19

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
+// @ts-ignore
 import React from 'react';
 import './App.css';
 import {Header} from "./components/Header/Header";
 import {Profile} from "./components/Profile/Profile";
 import {Navbar} from "./components/Navbar/Navbar";
+import {Dialogs} from "./components/Dialogs/Dialogs";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 
 
 function App() {
@@ -10,8 +13,18 @@ function App() {
         <div className='app-wrapper'>
             <Header/>
             <Navbar/>
-            <Profile/>
-        </div>
+
+
+                    <BrowserRouter>
+                        <div className={'app-wrapper-content'}>
+                    <Routes>
+                        <Route path="/profile" element={<Profile/>}/>
+                        <Route path="/dialogs" element={<Dialogs/>}/>
+                    </Routes>
+                        </div>
+                        </BrowserRouter>
+                </div>
+       
     );
 }
 
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -5,17 +5,27 @@ import Classes from './Navbar.module.css'
 // let c1 = 'item'
 // let c2 = 'active'
 // let classes = c1 + ' ' + c2 //'item active'
- let classesNew = `${Classes.item} ${Classes.active}`
+let classesNew = `${Classes.item} ${Classes.active}`
 
 export const Navbar = () => {
     return (
-        <nav className= {Classes.nav}>
-            <div className = {classesNew}>
-                <div>  Profile</div>
-            <div>Messages</div>
-            <div>New</div>
-            <div>Music</div>
-            <div>Settings</div>
+        <nav className={Classes.nav}>
+            <div className={classesNew}>
+                <div>
+                    <a href='/profile'>Profile </a>
+                </div>
+                <div>
+                    <a href='/dialogs'>Messages</a>
+                </div>
+                <div>
+                    <a href='/new'>New</a>
+                    </div>
+                <div>
+                    <a href='/music'>Music</a>
+                </div>
+                <div>
+                    <a href='/settings'>Settings</a>
+                </div>
             </div>
         </nav>
     )

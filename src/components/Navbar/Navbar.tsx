ae92813513a218f16cf444e4ba26eb38948c8e9a
EvgenyLukashevich <info.elukas@gmail.com>
2023-05-23 10:57:43 +0300

    Lesson-21

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -11,26 +11,35 @@ let classesNew = `${Classes.item} ${Classes.active}`
 export const Navbar = () => {
     return (
         <nav className={Classes.nav}>
-            <div className={classesNew}>
+            <div className={Classes.item}>
                 <div>
-                    <NavLink to={'/profile'}>Profile </NavLink>
+                    <NavLink to={'/profile'}
+                             className={navData => navData.isActive ? Classes.activeLink : Classes.item}>Profile </NavLink>
                     {/*<a href={'/profile'}>Profile </a>*/}
                 </div>
                 <div>
-                    {/*<NavLink to='/dialogs'>Messages</NavLink>*/}
-                    <a href='/dialogs'>Messages</a>
+                    <NavLink to='/dialogs'
+                             className={navData => navData.isActive ? Classes.activeLink : Classes.item}
+                    >Messages</NavLink>
+                    {/*<a href='/dialogs'>Messages</a>*/}
                 </div>
                 <div>
-                    {/*<NavLink to='/new'>New</NavLink>*/}
-                    <a href='/new'>New</a>
-                    </div>
-                <div>
-                    {/*<NavLink to='/music'>Music</NavLink>*/}
-                    <a href='/music'>Music</a>
+                    <NavLink to='/new'
+                             className={navData => navData.isActive ? Classes.activeLink : Classes.item}
+                    >New</NavLink>
+                    {/*<a href='/new'>New</a>*/}
                 </div>
                 <div>
-                    {/*<NavLink to='/settings'>Settings</NavLink>*/}
-                    <a href='/settings'>Settings</a>
+                    <NavLink to='/music'
+                             className={navData => navData.isActive ? Classes.activeLink : Classes.item}
+                    >Music</NavLink>
+                    {/*<a href='/music'>Music</a>*/}
+                </div>
+                <div>
+                    <NavLink to='/settings'
+                             className={navData => navData.isActive ? Classes.activeLink : Classes.item}
+                    >Settings</NavLink>
+                    {/*<a href='/settings'>Settings</a>*/}
                 </div>
             </div>
         </nav>
